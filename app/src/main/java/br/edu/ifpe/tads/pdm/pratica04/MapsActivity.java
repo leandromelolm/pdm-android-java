e97fdf21e467927a4ce3b6c3b5ea80c0f5ea837d
Leandro Melo <lsm@a.recife.ifpe.edu.br>
2021-08-23 10:23:09 -0300

    Pratica04-Maps Parte 3: Adicionando e trabalhando com marcadores

--- app/src/main/java/br/edu/ifpe/tads/pdm/pratica04/MapsActivity.java
+++ app/src/main/java/br/edu/ifpe/tads/pdm/pratica04/MapsActivity.java
@@ -3,13 +3,18 @@ package br.edu.ifpe.tads.pdm.pratica04;
 import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.Date;
 
 import br.edu.ifpe.tads.pdm.pratica04.databinding.ActivityMapsBinding;
 
@@ -43,10 +48,40 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        LatLng recife = new LatLng(-8.05, -34.9);
+        LatLng caruaru = new LatLng(-8.27, -35.98);
+        LatLng joaopessoa = new LatLng(-7.12, -34.84);
+        mMap.addMarker( new MarkerOptions().
+                position(recife).
+                title("Recife").
+                icon(BitmapDescriptorFactory.defaultMarker(35)));
+        mMap.addMarker( new MarkerOptions().
+                position(caruaru).
+                title("Caruaru").
+                icon(BitmapDescriptorFactory.defaultMarker(120)));
+        mMap.addMarker( new MarkerOptions().
+                position(joaopessoa).
+                title("João Pessoa").
+                icon(BitmapDescriptorFactory.defaultMarker(230)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(recife));
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                Toast.makeText(MapsActivity.this,
+                        "Você clicou em " + marker.getTitle(),
+                        Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                mMap.addMarker(new MarkerOptions().
+                        position(latLng).
+                        title("Adicionado em " + new Date()).
+                        icon(BitmapDescriptorFactory.defaultMarker(0)));
+            }
+        });
     }
 }
