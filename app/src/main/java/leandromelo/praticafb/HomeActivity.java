a133648e5c17cd18baa00f476c54d383492e23d5
Leandro Melo <lsm@a.recife.ifpe.edu.br>
2021-08-23 19:02:45 -0300

    Pratica06 Parte 5: Conectando a interface com o Firebase

--- app/src/main/java/leandromelo/praticafb/HomeActivity.java
+++ app/src/main/java/leandromelo/praticafb/HomeActivity.java
@@ -4,15 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import leandromelo.praticafb.model.Message;
+import leandromelo.praticafb.model.User;
 
 public class HomeActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private FirebaseAuthListener authListener;
+    private ViewGroup vgChat;
+    private DatabaseReference drUser;
+    private DatabaseReference drChat;
+    private EditText edMessage;
+    private User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +38,49 @@ public class HomeActivity extends AppCompatActivity {
 
         this.mAuth = FirebaseAuth.getInstance();
         this.authListener = new FirebaseAuthListener(this);
+
+        vgChat = findViewById(R.id.chat_area);
+        edMessage = findViewById(R.id.edit_message);
+
+        User user = new User();
+
+        FirebaseDatabase fbDB = FirebaseDatabase.getInstance();
+        FirebaseUser fbUser = mAuth.getCurrentUser();
+        drUser = fbDB.getReference("users/" + fbUser.getUid());
+        drChat = fbDB.getReference("chat");
+        drUser.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                User tempUser = dataSnapshot.getValue(User.class);
+               if (tempUser != null) {
+                    HomeActivity.this.user = tempUser;
+                   TextView txWelcome = (TextView)findViewById(R.id.tx_welcome);
+                    txWelcome.setText("Welcome " + tempUser.getName() + "!");
+                }
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+        drChat.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Message message = dataSnapshot.getValue(Message.class);
+                showMessage(message);
+            }
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) { }
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) { }
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) { }
+            @Override
+            public void onCancelled(DatabaseError databaseError) { }
+
+
+
+        });
+
     }
 
     public void buttonSignOutClick(View view) {
@@ -44,4 +104,18 @@ public class HomeActivity extends AppCompatActivity {
         super.onStop();
         mAuth.removeAuthStateListener(authListener);
     }
+
+    private void showMessage(Message message) {
+        TextView tvMsg = new TextView(this);
+        tvMsg.setText(message.getName() + ": " + message.getText());
+
+        vgChat.addView(tvMsg);
+    }
+
+    public void buttonSendMsgClick(View view) {
+        String message = edMessage.getText().toString();
+        edMessage.setText("");
+        drChat.push().setValue(new Message(user.getName(), message));
+    }
+
 }
